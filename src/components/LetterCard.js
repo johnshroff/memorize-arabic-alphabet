e41e7bea19569011d335e8991fb274774181fe95
John Shroff <john.shroff@flowtrac.com>
2020-09-13 10:17:04 -0500

    add loading spinner for sounds fix improper letter assignment

--- src/components/LetterCard.js
+++ src/components/LetterCard.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
 import {Card} from "primereact/card";
 import {Button} from "primereact/button";
+import {ProgressSpinner} from 'primereact/progressspinner';
+import {Dialog} from 'primereact/dialog';
 
 export function LetterCard(props) {
 
 	const {letter, onNextClick} = props;
 	const [showLetter, setShowLetter] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-	function playSound() {
-		const audio = new Audio(`../assets/sounds/${letter}.mp3`);
-		audio.play();
+	async function playSound() {
+		setLoading(true);
+		try {
+			const audio = new Audio(`../assets/sounds/${letter}.mp3`);
+			await audio.play();
+		} finally {
+			setLoading(false);
+		}
 	}
 
 	function toggleLetter() {
@@ -28,6 +36,9 @@ export function LetterCard(props) {
 		<Card footer={footer}>
 			<h2 style={showLetter ? {} : {visibility: 'hidden'}}>{letter.toUpperCase()}</h2>
 			<img src={`../assets/images/${letter}.png`} alt={`card-${letter}-image`}/>
+			<Dialog visible={loading} modal onHide={() => {}} showHeader={false}>
+				<ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8"/>
+			</Dialog>
 		</Card>
 	);
 };
