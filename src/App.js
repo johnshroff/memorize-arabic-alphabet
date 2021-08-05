85417c3bcdcc328d5006baf24c0aad5b4592f665
John Shroff <john.shroff@flowtrac.com>
2021-08-05 12:39:21 -0500

    move css imports to index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, {useState, useRef} from 'react';
 import './App.css';
-import 'primereact/resources/themes/nova/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
 
 import {LetterCard} from "./components/LetterCard";
 import {MainMenu} from "./components/MainMenu";
