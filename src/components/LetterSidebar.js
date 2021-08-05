d6cd9ab2b1c70030e4ddad9364b9d37b082e94a5
John Shroff <john.shroff@flowtrac.com>
2021-08-05 12:35:33 -0500

    fix key prop warning

--- src/components/LetterSidebar.js
+++ src/components/LetterSidebar.js
@@ -4,7 +4,7 @@ import {Button} from 'primereact/button';
 import alphabet from '../lib/alphabet';
 
 
-const Letter = ({selected, letter, imageSize, onClick, key}) => {
+const Letter = ({selected, letter, imageSize, onClick}) => {
 	const style = {
 		cursor: 'pointer',
 		height: imageSize,
